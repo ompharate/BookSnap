5f8185c8cfa9300d0c8495bfd2677b48a4711ad9
ompharate <ompharate31@gmail.com>
2024-08-14 17:21:45 +0530

    update url 3

--- Server/app.js
+++ Server/app.js
@@ -5,22 +5,25 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
-
+// Use CORS middleware before defining routes
 const corsOptions = {
-  origin: "https://book-snap-frontend.vercel.app", // Replace with your frontend URL
+  origin: "https://book-snap-frontend.vercel.app", 
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
+// Middleware setup
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route setup
 const authRoutes = require("./routes/auth");
 app.use("/api", authRoutes);
 
+// Define port and start server
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-  console.log(`App is running at ${port} http://localhost:3000`);
+  console.log(`App is running at http://localhost:${port}`);
 });
